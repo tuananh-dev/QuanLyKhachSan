ac16724ac76ffb45352683716a48631cb0d14090
PQDuc <oneduc189@gmail.com>
2020-06-26 15:20:55 +0700

    update danh sách đoàn update danh sách đặt phòng thành công update danh sách đặt phòng thất bại

--- QLKSProject/QLKSProject/Controllers/NhanVien/NhanVienController.cs
+++ QLKSProject/QLKSProject/Controllers/NhanVien/NhanVienController.cs
@@ -5,11 +5,36 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using QLKSProject.Business.NhanVien;
+using QLKSProject.Models;
+using QLKSProject.Models.Entities;
 
 namespace QLKSProject.Controllers.NhanVien
 {
     public class NhanVienController : ApiController
     {
-
+        [HttpPost]
+        public List<Doan> LayDanhSachDoan()
+        {
+            using (NhanVienBusiness nhanvien = new NhanVienBusiness())
+            {
+                return nhanvien.LayDanhSachDoan();
+            }
+        }
+        [HttpPost]
+        public List<DatPhongThanhCong> LayDanhSachDatPhongThanhCong()
+        {
+            using(NhanVienBusiness datphongthanhcong =new NhanVienBusiness())
+            {
+                return datphongthanhcong.LayDanhSachDatPhongThanhCong();
+            }
+        }
+        [HttpPost]
+        public List<DatPhongThatBai> LayDanhSachDatPhongThatBai()
+        {
+            using (NhanVienBusiness datphongthatbai = new NhanVienBusiness())
+            {
+                return datphongthatbai.LayDanhSachDatPhongThatBai();
+            }
+        }
     }
 }
