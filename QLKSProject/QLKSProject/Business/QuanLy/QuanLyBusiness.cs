d062d122b4acb137ba9b81793f90f6cb0d8d6d27
namNNDT <namnguyen.nn369@gmail.com>
2020-06-26 15:00:41 +0700

    QuanLyController

--- QLKSProject/QLKSProject/Business/QuanLy/QuanLyBusiness.cs
+++ QLKSProject/QLKSProject/Business/QuanLy/QuanLyBusiness.cs
@@ -1,13 +1,99 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using QLKSProject.Models;
 
 namespace QLKSProject.Business.QuanLy
 {
+    //TaiKhoan
     public class QuanLyBusiness : BaseBusiness
     {
-        
+        public List<TaiKhoan> LayDanhSachTaiKhoan()
+        {
+            var taikhoan = models.TaiKhoans.Where(e => e.IsDelete == false).Select(e => e).ToList();
+            return taikhoan;
+        }
+
+        public bool ThemTaiKhoan(TaiKhoan taikhoan)
+        {
+            try
+            {
+                models.TaiKhoans.Add(taikhoan);
+                models.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+
+        //PHONG
+        public List<Phong> LayDanhSachPhong()
+        {
+            var phong = models.Phongs.Where(e => e.IsDelete == false).Select(e => e).ToList();
+            return phong;
+        }
+        public bool ThemPhong(Phong phong)
+        {
+            try
+            {
+                models.Phongs.Add(phong);
+                models.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+
+
+        ////DichVu
+        //public List<DichVu> LayDanhSachDichVu()
+        //{
+        //    var dichvu = models.DichVus.Where(e => e.IsDelete == false).Select(e => e).ToList();
+        //    return dichvu;
+        //}
+        //public bool ThemDichVu(DichVu dichvu)
+        //{
+        //    try
+        //    {
+        //        models.DichVus.Add(dichvu);
+        //        models.SaveChanges();
+        //        return true;
+        //    }
+        //    catch (Exception)
+        //    {
+        //        return false;
+        //    }
+
+        //}
+
+
+        ////Tien Ich
+        //public List<TienIch> LayDanhSachTienIch()
+        //{
+        //    var tienich = models.TienIches.Where(e => e.IsDelete == false).Select(e => e).ToList();
+        //    return tienich;
+        //}
+        //public bool ThemTienIch(TienIch tienich)
+        //{
+        //    try
+        //    {
+        //        models.TienIches.Add(tienich);
+        //        models.SaveChanges();
+        //        return true;
+        //    }
+        //    catch (Exception)
+        //    {
+        //        return false;
+        //    }
+
+        //}
     }
 }
--- QLKSProject/QLKSProject/Controllers/QuanLy/QuanLyController.cs
+++ QLKSProject/QLKSProject/Controllers/QuanLy/QuanLyController.cs
@@ -1,18 +1,93 @@
-﻿using System;
+﻿using QLKSProject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using QLKSProject.Business.QuanLy;
 
 namespace QLKSProject.Controllers.QuanLy
 {
     public class QuanLyController : ApiController
     {
-        [HttpPost]
-        public string Name()
+
+        //TAIKHOAN
+        [HttpGet]
+        public List<TaiKhoan> LayDanhSachTaiKhoan()
         {
-            return "xxxxx";
+            using (QuanLyBusiness quanLy = new QuanLyBusiness())
+            {
+
+                return quanLy.LayDanhSachTaiKhoan() ;
+            }
         }
+        [HttpPost]
+        public bool ThemTaiKhoan(TaiKhoan taikhoan)
+        {
+            using (QuanLyBusiness quanLy = new QuanLyBusiness())
+            {
+                quanLy.ThemTaiKhoan(taikhoan);
+                return true;
+            }
+        }
+
+        //PHONG
+        [HttpGet]
+        public List<Phong> LayDanhSachPhong()
+        {
+            using (QuanLyBusiness quanLy = new QuanLyBusiness())
+            {
+
+                return quanLy.LayDanhSachPhong();
+            }
+        }
+        [HttpPost]
+        public bool ThemPhong(Phong phong)
+        {
+            using (QuanLyBusiness quanLy = new QuanLyBusiness())
+            {
+                quanLy.ThemPhong(phong);
+                return true;
+            }
+        }
+        ////DICHVU
+        //[HttpGet]
+        //public List<DichVu> LayDanhSachDichVu()
+        //{
+        //    using (QuanLyBusiness quanLy = new QuanLyBusiness())
+        //    {
+
+        //        return quanLy.LayDanhSachDichVu();
+        //    }
+        //}
+        //[HttpPost]
+        //public bool ThemDichVu(DichVu dichvu)
+        //{
+        //    using (QuanLyBusiness quanLy = new QuanLyBusiness())
+        //    {
+        //        quanLy.ThemDichVu(dichvu);
+        //        return true;
+        //    }
+        //}
+        ////TIENICH
+        //[HttpGet]
+        //public List<TienIch> LayDanhSachTienIch()
+        //{
+        //    using (QuanLyBusiness quanLy = new QuanLyBusiness())
+        //    {
+
+        //        return quanLy.LayDanhSachTienIch();
+        //    }
+        //}
+        //[HttpPost]
+        //public bool ThemTienIch(TienIch tienich)
+        //{
+        //    using (QuanLyBusiness quanLy = new QuanLyBusiness())
+        //    {
+        //        quanLy.ThemTienIch(tienich);
+        //        return true;
+        //    }
+        //}
     }
 }
