2c4a6d52e0bf333301d3a6bd0ad14e53b5da918e
kiemphongkim <kiemphongkim911@gmail.com>
2020-07-01 17:13:38 +0700

    Xắp xếp lại vị trí file KhoiTaoDuLieu, thiết lập trang home

--- QLKSProject/QLKSProject/Controllers/Home/HomeController.cs
+++ QLKSProject/QLKSProject/Controllers/Home/HomeController.cs
@@ -4,21 +4,15 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using QLKSProject.Models;
-using QLKSProject.Business.Home;
+
 
 namespace QLKSProject.Controllers.Home
 {
     public class HomeController : ApiController
     {
-/*        [HttpPost]
-        public bool LayFileKhachHangGui(FileKhachHang fileKhachHang)
+/*        [HttpGet]
+        public IHttpActionResult Home()
         {
-            using (HomeBusiness homeBusiness = new HomeBusiness())
-            {
-                
-                return homeBusiness.LuuDuLieuXuongCSDL(fileKhachHang);
-            }       
         }*/
     }
 }
--- QLKSProject/QLKSProject/Global.asax.cs
+++ QLKSProject/QLKSProject/Global.asax.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
-
 namespace QLKSProject
 {
     public class WebApiApplication : System.Web.HttpApplication
