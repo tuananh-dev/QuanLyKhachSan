09eb65e697f579d0ec6ab45ec7c8f2f25dcfb9e6
ngvanloi <nguyenloi.itse@gmail.com>
2020-07-16 11:28:41 +0700

    add file libs_js

---
+++ QLKSProject/QLKSProject/Content/QuanLy/libs_function.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+
+})
+
+function loadTienIch() {
+    var mota = $('#dsmota');
+
+    $.ajax({
+        type: 'GET',
+        url: '/api/Quanly/LayDanhSachTienIch',
+        dataType: 'json',
+        success: function (data) {
+            mota.empty();
+
+            $.each(data, function (index, val) {
+
+                mota.prepend('<tr class="odd gradeX"><td class="center">' + val.TenTienIch + '</td><td class="center">' + val.MoTa + '</td><td class="center"><a class="btn btn-tbl-edit btn-xs" data-id="' + val.ID + '" ><i class="fa fa-pencil"></i></a><a class="btn btn-tbl-delete btn-xs" data-id="' + val.ID + '"><i class="fa fa-trash-o "></i></a></td></tr>');
+
+            });
+
+        }
+    });
+}
