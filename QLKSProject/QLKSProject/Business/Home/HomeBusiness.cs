e094c0ce5cf61a237d0afe055b285517535e0c10
nhphuc99 <huyphuccpag@gmail.com>
2020-07-17 10:34:53 +0700

    Check Validate Form Gửi Danh Sách Đoàn

--- QLKSProject/QLKSProject/Business/Home/HomeBusiness.cs
+++ QLKSProject/QLKSProject/Business/Home/HomeBusiness.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 using QLKSProject.Models.DTO;
 namespace QLKSProject.Business.Home
 {
-	public class HomeBusiness : BaseBusiness
-	{
-		public string TestDuLieuTruyenXuong(dynamic dynamic)
-		{
+    public class HomeBusiness : BaseBusiness
+    {
+        public string TestDuLieuTruyenXuong(dynamic dynamic)
+        {
             //using (System.IO.StreamWriter file =
             //new System.IO.StreamWriter(@"D:/WriteLines2.txt", false))
             //{
-               
+
             //    file.WriteLine(s);
             //    file.Close();
             //}
             string s = dynamic.ToString();
             return s;
-		}
-	}
+        }
+    }
 }
