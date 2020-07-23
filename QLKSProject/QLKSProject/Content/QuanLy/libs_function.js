27a14410f37440daffdc71a41a256c330326828d
ngvanloi <nguyenloi.itse@gmail.com>
2020-07-24 02:25:11 +0700

    them chuc nang dat phong View DSDoan, Hien thi DS View KH

--- QLKSProject/QLKSProject/Content/QuanLy/libs_function.js
+++ QLKSProject/QLKSProject/Content/QuanLy/libs_function.js
@@ -3,47 +3,58 @@
 //var themTienIch = 'Quanly/ThemTienIch';
 //var suaTienIch = 'Quanly/CapNhatTienIch';
 //var xoaTienIch = 'Quanly/XoaTienIch';
+//<th class="center"> Họ và Tên </th>
+//    <th class="center"> SĐT</th>
+//    <th class="center"> Nguời Đại Diện</th>
+//    <th class="center"> Phòng </th>
 
 
+function loadData(idList, url) {
+    var position = $(idList);
 
-//function loadData(idList, url) {
-//    var position = $(idList);
+    $.ajax({
+        type: 'GET',
+        url: '/api/' + url,
+        dataType: 'json',
+        success: function (data) {
+            position.empty();
 
-//    $.ajax({
-//        type: 'GET',
-//        url: '/api/' + url,
-//        dataType: 'json',
-//        success: function (data) {
-//            position.empty();
+            $.each(data, function (index, val) {
+                if (url == 'Quanly/LayDanhSachTienIch') {
+                    position.prepend('<tr class="odd gradeX"><td class="center">' + val.TenTienIch + '</td><td class="center">' + val.MoTa + '</td><td class="center"><a class="btn btn-tbl-edit btn-xs" data-id="' + val.ID + '" ><i class="fa fa-pencil"></i></a><a class="btn btn-tbl-delete btn-xs" data-id="' + val.ID + '"><i class="fa fa-trash-o "></i></a></td></tr>');
+                }
+                if (url == 'Quanly/LayDanhSachDichVu') {
+                    position.prepend('<tr class="odd gradeX"><td class="center">' + val.TenDichVu + '</td><td class="center">' + val.Gia + '</td><td class="center">' + val.MoTa + '</td><td class="center"><a class="btn btn-tbl-edit btn-xs" data-id="' + val.ID + '" ><i class="fa fa-pencil"></i></a><a class="btn btn-tbl-delete btn-xs" data-id="' + val.ID + '"><i class="fa fa-trash-o "></i></a></td></tr>');
+                }
+                if (url == 'Quanly/LayDanhSachPhong') {
+                    var tt = "Trống";
+                    if (val.TrangThai) {
+                        tt = "Đang sử dụng"
+                    }
+                    position.prepend('<tr class="odd gradeX"><td class="center">' + val.MaPhong + '</td><td class="center">' + val.SoPhong + '</td><td class="center">' + val.LoaiPhong + '</td><td class="center">' + val.Gia + '</td><td class="center">' + tt + '</td><td class="center"><a class="btn btn-tbl-edit btn-xs btn-edit" data-id="' + val.ID + '"><i class="fa fa-pencil"></i></><a class="btn btn-tbl-delete btn-xs btn-delete" data-id="' + val.ID + '"><i class="fa fa-trash-o "></i></a></td></tr>');
+                }
+                if (url == 'Quanly/LayDanhSachTaiKhoan') {
+                    position.prepend('<tr class="odd gradeX"><td class="center">' + val.TenTaiKhoan + '</td><td class="center">' + val.MatKhau + '</td><td class="center">' + val.HoVaTen + '</td><td class="center">' + val.SoDienThoai + '</td><td class="center">' + val.Mail + '</td><td class="center">' + val.LoaiTaiKhoan + '</td><td class="center"><a class="btn btn-tbl-edit btn-xs" data-id="' + val.ID + '"><i class="fa fa-pencil"></i></a><a class="btn btn-tbl-delete btn-xs" data-id="' + val.ID + '"><i class="fa fa-trash-o "></i></a></td></tr>');
+                }
+                if (url == 'NhanVien/LayDanhSachDoan') {
+                    var tgNhan = new Date(val.ThoiGianNhan);
+                    var tgTra = new Date(val.ThoiGianTra);
+                    var ngayGui = new Date(val.NgayGui);
+                    position.prepend('<tr class="odd gradeX"><td class="center">' + val.TenDoan + '</td><td class="center">' + val.TenTruongDoan + '</td><td class="center">' + tgNhan.getDate() + '-' + (tgNhan.getMonth() + 1) + '-' + tgNhan.getFullYear() + '</td><td class="center">' + tgTra.getDate() + '-' + (tgTra.getMonth() + 1) + '-' + tgTra.getFullYear() + '</td><td class="center">' + ngayGui.getDate() + '-' + (ngayGui.getMonth() + 1) + '-' + ngayGui.getFullYear() + '</td><td class="center"><a class="btn btn-info btn-xs" data-id="' + val.MaDoan + '" style="border-radius: 40%" ><i class="fa fa-pencil"></i></a></td></tr>');
+                }
 
-//            $.each(data, function (index, val) {
-//                if (url == 'Quanly/LayDanhSachTienIch') {
-//                    position.prepend('<tr class="odd gradeX"><td class="center">' + val.TenTienIch + '</td><td class="center">' + val.MoTa + '</td><td class="center"><a class="btn btn-tbl-edit btn-xs" data-id="' + val.ID + '" ><i class="fa fa-pencil"></i></a><a class="btn btn-tbl-delete btn-xs" data-id="' + val.ID + '"><i class="fa fa-trash-o "></i></a></td></tr>');
-//                }
-//                if (url == 'Quanly/LayDanhSachDichVu') {
-//                    position.prepend('<tr class="odd gradeX"><td class="center">' + val.TenDichVu + '</td><td class="center">' + val.Gia + '</td><td class="center">' + val.MoTa + '</td><td class="center"><a class="btn btn-tbl-edit btn-xs" data-id="' + val.ID + '" ><i class="fa fa-pencil"></i></a><a class="btn btn-tbl-delete btn-xs" data-id="' + val.ID + '"><i class="fa fa-trash-o "></i></a></td></tr>');
-//                }
-//                if (url == 'Quanly/LayDanhSachPhong') {
-//                    var tt = "Trống";
-//                    if (val.TrangThai) {
-//                        tt = "Đang sử dụng"
-//                    }
-//                    position.prepend('<tr class="odd gradeX"><td class="center">' + val.MaPhong + '</td><td class="center">' + val.SoPhong + '</td><td class="center">' + val.LoaiPhong + '</td><td class="center">' + val.Gia + '</td><td class="center">' + tt + '</td><td class="center"><a class="btn btn-tbl-edit btn-xs btn-edit" data-id="' + val.ID + '"><i class="fa fa-pencil"></i></><a class="btn btn-tbl-delete btn-xs btn-delete" data-id="' + val.ID + '"><i class="fa fa-trash-o "></i></a></td></tr>');
-//                }
-//                if (url == 'Quanly/LayDanhSachTaiKhoan') {
-//                    position.prepend('<tr class="odd gradeX"><td class="center">' + val.TenTaiKhoan + '</td><td class="center">' + val.MatKhau + '</td><td class="center">' + val.HoVaTen + '</td><td class="center">' + val.SoDienThoai + '</td><td class="center">' + val.Mail + '</td><td class="center">' + val.LoaiTaiKhoan + '</td><td class="center"><a class="btn btn-tbl-edit btn-xs" data-id="' + val.ID + '"><i class="fa fa-pencil"></i></a><a class="btn btn-tbl-delete btn-xs" data-id="' + val.ID + '"><i class="fa fa-trash-o "></i></a></td></tr>');
-//                }
-//                if (url == 'NhanVien/LayDanhSachDoan') {
-//                    var tgNhan = new Date(val.ThoiGianNhan);
-//                    var tgTra = new Date(val.ThoiGianTra);
-//                    var ngayGui = new Date(val.NgayGui);
-//                    position.prepend('<tr class="odd gradeX"><td class="center">' + val.TenDoan + '</td><td class="center">' + val.TenTruongDoan + '</td><td class="center">' + tgNhan.getDate() + '-' + (tgNhan.getMonth() + 1) + '-' + tgNhan.getFullYear() + '</td><td class="center">' + tgTra.getDate() + '-' + (tgTra.getMonth() + 1) + '-' + tgTra.getFullYear() + '</td><td class="center">' + ngayGui.getDate() + '-' + (ngayGui.getMonth() + 1) + '-' + ngayGui.getFullYear() + '</td><td class="center"><a class="btn btn-tbl-edit btn-xs" data-id="' + val.ID + '" ><i class="fa fa-pencil"></i></a></td></tr>');
-//                }
-//            });
+                if (url == 'KhachHang/LayDanhSachKhachHangTheoMaDoan/1595555103849') {
+                    var daidien = '';
+                    if (val.NguoiDaiDienCuaTreEm != '0') {
+                        daidien = val.NguoiDaiDienCuaTreEm;
+                    }
+                    position.prepend('<tr class="odd gradeX"><td class="center">' + val.HoVaTen + '</td><td class="center">' + val.SoDienThoai + '</td><td class="center">' + daidien + '</td><td class="center">' + val.IDPhong + '</td></tr>');
+                }
+            });
 
-//        }
-//    });
-//}
+        }
+    });
+}
 
 function loadDataDetail(edit, url, id) {
 
@@ -65,7 +76,7 @@ function loadDataDetail(edit, url, id) {
                     $(edit.gia).val(data.Gia);
                     break;
                 case 'Quanly/LayPhong?ID=':
-                    var loaiPhong ;
+                    var loaiPhong;
                     if (data.LoaiPhong == "1") {
                         loaiPhong = "Phòng Loại 1";
                     }
@@ -189,4 +200,23 @@ function deleteData(info, dataInput) {
             loadData(info.id, info.urlLoad);
         }
     })
+}
+
+
+function datPhong(info, url) {
+    $.ajax({
+        type: 'GET',
+        url: '/api/' + url,
+        headers: { 'content-type': 'application/json', 'data-type': 'json' },
+        dataType: 'json',
+        success: function (data) {
+            alert(data);
+
+        },
+        error: function (data) {
+            alert(data.responseJSON.Message)
+
+        }
+    })
+
 }
