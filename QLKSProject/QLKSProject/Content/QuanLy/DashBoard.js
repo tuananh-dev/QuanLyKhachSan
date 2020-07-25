9536190d50d72955f70f0fdbe9ac6c4a5f383df3
ngvanloi <nguyenloi.itse@gmail.com>
2020-07-25 21:19:11 +0700

    update thongke

--- QLKSProject/QLKSProject/Content/QuanLy/DashBoard.js
+++ QLKSProject/QLKSProject/Content/QuanLy/DashBoard.js
@@ -81,12 +81,20 @@ function dashboard_Circle(url) {
                 "nam": 2020
             }),
             success: function (data) {
+
+                var obj = [];
+                $.each(data, function (index, val) {
+                    var rnd = Math.floor(Math.random() * 120) + 70;
+                    obj.push({ name: val.TenDichVu, y: val.DoanThu, z: rnd });
+
+                });
+
                 Highcharts.chart('circle', {
                     chart: {
                         type: 'variablepie'
                     },
                     title: {
-                        text: 'Thống Kê Doanh Thu Tổng Quát'
+                        text: 'Thống Kê Doanh Thu Theo Tháng'
                     },
                     tooltip: {
                         headerFormat: '',
@@ -98,12 +106,7 @@ function dashboard_Circle(url) {
                         innerSize: '20%',
                         zMin: 0,
                         name: 'countries',
-                        data: [
-                            { name: data[0].TenDichVu, y: data[0].DoanThu, z: 92.9 },
-                            { name: data[1].TenDichVu, y: data[1].DoanThu, z: 118.7 },
-                            { name: data[2].TenDichVu, y: data[2].DoanThu, z: 124.6 },
-                            { name: data[3].TenDichVu, y: data[3].DoanThu, z: 137.5 }
-                        ]
+                        data: obj
                     }]
                 });
             }
@@ -111,74 +114,54 @@ function dashboard_Circle(url) {
     })
 }
 
-function dashboard_ThreeBar(url) {
-    Highcharts.chart('three-bar', {
-        title: {
-            text: 'Combination chart'
-        },
-        xAxis: {
-            categories: ['Thuê Phòng', 'Massage', 'Pears', 'Bananas', 'Plums']
-        },
-        labels: {
-            items: [{
-                html: 'Total fruit consumption',
-                style: {
-                    left: '50px',
-                    top: '18px',
-                    color: ( // theme
-                        Highcharts.defaultOptions.title.style &&
-                        Highcharts.defaultOptions.title.style.color
-                    ) || 'black'
-                }
-            }]
-        },
-        series: [{
-            type: 'column',
-            name: 'Jane',
-            data: [3, 2, 1, 3, 4]
-        }, {
-            type: 'column',
-            name: 'John',
-            data: [2, 3, 5, 7, 6]
-        }, {
-            type: 'column',
-            name: 'Joe',
-            data: [4, 3, 3, 9, 0]
-        }, {
-            type: 'spline',
-            name: 'Average',
-            data: [3, 2.67, 3, 6.33, 3.33],
-            marker: {
-                lineWidth: 2,
-                lineColor: Highcharts.getOptions().colors[3],
-                fillColor: 'white'
-            }
-        }, {
-            type: 'pie',
-            name: 'Total consumption',
-            data: [{
-                name: 'Jane',
-                y: 13,
-                color: Highcharts.getOptions().colors[0] // Jane's color
-            }, {
-                name: 'John',
-                y: 23,
-                color: Highcharts.getOptions().colors[1] // John's color
-            }, {
-                name: 'Joe',
-                y: 19,
-                color: Highcharts.getOptions().colors[2] // Joe's color
-            }],
-            center: [100, 80],
-            size: 100,
-            showInLegend: false,
-            dataLabels: {
-                enabled: false
-            }
-        }]
-    });
+//function dashboard_ThreeBar(url) {
+//    $(document).ready(function () {
+//        $.ajax({
+//            type: 'POST',
+//            url: '/api/' + url,
+//            headers: { 'content-type': 'application/json', 'data-type': 'json' },
+//            data: JSON.stringify({
+               
+//            }),
+//            success: function (data) {
 
-}
+//                var obj = [];
+//                $.each(data, function (index, val) { })
+                
 
+//            }
+//        })
+//    })
+//}
 
+Highcharts.chart('three-bar', {
+    title: {
+        text: 'Thống Kê Doanh Thu Theo Quý'
+    },
+    xAxis: {
+        categories: ['Thuê Phòng', 'Massage', 'Thuê Xe', 'Giặt Ủi', 'Ăn Uống']
+    },
+    series: [{
+        type: 'column',
+        name: 'Tháng 1',
+        data: [3, 2, 1, 3, 4]
+    }, {
+        type: 'column',
+        name: 'Tháng 2',
+        data: [2, 3, 5, 7, 6]
+    }, {
+        type: 'column',
+        name: 'Tháng 3',
+        data: [4, 3, 3, 9, 1]
 
+    }, {
+        type: 'spline',
+        name: 'Trung Bình',
+        data: [3, 2.67, 3, 6.33, 3.33],
+        marker: {
+            lineWidth: 2,
+            lineColor: Highcharts.getOptions().colors[3],
+            fillColor: 'white'
+        }
+    }]
+});
