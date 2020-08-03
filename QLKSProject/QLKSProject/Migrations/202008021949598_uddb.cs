2793bfa05aec5477792e1eb7706a773e0ab4e801
kiemphongkim <kiemphongkim911@gmail.com>
2020-08-03 14:34:07 +0700

    no message

---
+++ QLKSProject/QLKSProject/Migrations/202008021949598_uddb.cs
@@ -0,0 +1,16 @@
+﻿namespace QLKSProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class uddb : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
